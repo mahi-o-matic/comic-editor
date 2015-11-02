e2a661248c2f1a5f9b70c3956d4514ac84a439df
Lars Güttinger <Eriklars@gmx.de>
2015-11-02 17:45:05 +0100

    Standard Zeug
    Bla

--- Assets/Scripts/CanvasHandler.cs
+++ Assets/Scripts/CanvasHandler.cs
@@ -2,17 +2,30 @@
 using System.Collections;
 using System.IO;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CanvasHandler : MonoBehaviour
 {
     public InputField Pathfield;
-    private string path;
-	public void PathTest()
+    public Image image;
+    private ComicChapter comicChapter = new ComicChapter();
+    public EventSystem eventsystem;
+
+    public void Awake()
     {
-        path = Pathfield.text;
-        if (!File.Exists(path))
+        eventsystem.SetSelectedGameObject(Pathfield.gameObject);
+    }
+
+    public void Update()
+    {
+        if (File.Exists(Pathfield.text))
         {
-            Debug.Log(path);
+            image.color = Color.green;
+        }
+        else
+        {
+            image.color = Color.red;
+            Debug.Log(Pathfield.text);
             Debug.LogWarning("File didnt exist");
         }
     }
